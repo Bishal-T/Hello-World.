d4661ea737d390d3c86e4b35c0dad8e13f8e7df8
BuildTools <unconfigured@null.spigotmc.org>
2021-02-04 13:26:27 -0500

    Age and Name
    Added two int and string varible to assign the age 15 and name John.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,15 @@
 public class HelloWorld {
    public static void main(String[] args ) {
-   
+     
+      int age = 15;
+      String name = "John";
+         
       System.out.println("Hello World");
+      
+      System.out.println(age);
+      System.out.println(name);
    
   }
  }
+   
+ 
