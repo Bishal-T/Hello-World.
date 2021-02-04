17847a0c0145afc00cf0729d0bd757304890687e
BuildTools <unconfigured@null.spigotmc.org>
2021-02-04 13:33:21 -0500

    Revert "Age and Name"
    This reverts commit d4661ea737d390d3c86e4b35c0dad8e13f8e7df8.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,15 +1,7 @@
 public class HelloWorld {
    public static void main(String[] args ) {
-     
-      int age = 15;
-      String name = "John";
-         
+   
       System.out.println("Hello World");
-      
-      System.out.println(age);
-      System.out.println(name);
    
   }
  }
-   
- 
